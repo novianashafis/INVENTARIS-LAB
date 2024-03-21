ef1f1455c29c4836e1069be8084dee322f3c938c
Hafis <novianashafidz@gmail.com>
2024-03-21 21:38:40 +0700

    belum selesai

---
+++ main.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const inputAlat = document.getElementById('inputAlat');
+  const kotakAlat = [];
+  const RENDER_EVENT = 'render-list'
+})
